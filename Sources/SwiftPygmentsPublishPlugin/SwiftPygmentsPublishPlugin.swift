133be88fe261f093bb99e67f8895f4fb1f61049f
Zhijin Chen <mail@chenzhijin.com>
2020-01-22 14:32:13 +0900

    Update to support ```` as code block wrapper

--- Sources/SwiftPygmentsPublishPlugin/SwiftPygmentsPublishPlugin.swift
+++ Sources/SwiftPygmentsPublishPlugin/SwiftPygmentsPublishPlugin.swift
@@ -3,6 +3,8 @@ import Publish
 import Ink
 
 public extension Plugin {
+    
+    /// Pygments Plugin.
     static func pygments() -> Self {
         Plugin(name: "Pygments") { context in
             context.markdownParser.addModifier(
@@ -15,12 +17,21 @@ public extension Plugin {
 public extension Modifier {
     static func pygmentsCodeBlocks() -> Self {
         return Modifier(target: .codeBlocks) { html, markdown in
-            var markdown = markdown.dropFirst("```".count)
-
+            // Determine if the codeblock is wrapped by ``` or ````
+            var backticks : String
+            if markdown.hasPrefix("````") {
+                backticks = "````"
+            } else {
+                backticks = "```"
+            }
+            
+            var markdown = String(markdown.dropFirst(backticks.count))
+            
             guard !markdown.hasPrefix("no-highlight") else {
                 return html
             }
             
+            // Default: Swift
             var lexer : Pygments.Lexer = .swift
             
             if !markdown.hasPrefix("\n"),
@@ -29,22 +40,18 @@ public extension Modifier {
                 // Found language matching. Override default lexer.
                 lexer = aLexer
             }
-            markdown = markdown
+            markdown = String(markdown
                 .drop(while: { !$0.isNewline })
                 .dropFirst()
-                .dropLast("\n```".count)
-
+                .dropLast("\n\(backticks)".count)
+            )
+            
             var highlighted = Pygments.html(String(markdown), use: lexer)
             highlighted = highlighted.replacingOccurrences(of: "<pre>", with: "")
             highlighted = highlighted.replacingOccurrences(of: "</pre>", with: "")
             highlighted = highlighted.trimmingCharacters(in: .newlines)
+            
             return "<pre><code>" + highlighted + "</code></pre>"
         }
     }
-}
-
-
-struct SwiftPygmentsPublishPlugin {
-    var text = "Hello, World!"
-    
 }
